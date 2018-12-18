958a0c1145c3eb227c6c9de07dc0fb0847c53607
nayeon <ceoahn95@gmail.com>
2018-12-18 15:26:19 +0900

    Payment id 수정

--- app/src/main/java/com/hanki/hanki/ShopOrder/ShopPayment/PaymentOrderListViewHolder.java
+++ app/src/main/java/com/hanki/hanki/ShopOrder/ShopPayment/PaymentOrderListViewHolder.java
@@ -16,9 +16,9 @@ public class PaymentOrderListViewHolder extends RecyclerView.ViewHolder {
     public PaymentOrderListViewHolder(View itemView) {
         super(itemView);
 
-        VH_foodImage = (ImageView) itemView.findViewById(R.id.payment_foodImg);
-        VH_menuName = (TextView) itemView.findViewById(R.id.payment_shopTitle);
-        VH_menuSize = (TextView) itemView.findViewById(R.id.payment_foodSize);
-        VH_menuTopping = (TextView) itemView.findViewById(R.id.payment_topping);
+        VH_foodImage = (ImageView) itemView.findViewById(R.id.payment_MenuImg);
+        VH_menuName = (TextView) itemView.findViewById(R.id.payment_menuName);
+        VH_menuSize = (TextView) itemView.findViewById(R.id.payment_menuSize);
+        VH_menuTopping = (TextView) itemView.findViewById(R.id.payment_toppings);
     }
 }
